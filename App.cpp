497ea29c2b92dec81a3c31931635a927773df84d
Dylan <reachdylan@gmail.com>
2026-04-07 14:29:32 -0400

    Fixed MainMenu.cpp to properly initiliaze parent objects, no longer throwing errors. Cleaned up App.cpp, removed old experimental code. Commented out Destruction of old scene as it currently does not function correctly.

--- App.cpp
+++ App.cpp
@@ -14,19 +14,13 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit()
 {
     //ignore warning for now
-    wxSystemOptions::SetOption("msw.no-manifest-check", 1);
+    // wxSystemOptions::SetOption("msw.no-manifest-check", 1);
 
-    board = new Board();
+    // board = new Board();
 
-    //testing output
-    // wxString testOut;
-    //
-    // testOut += wxString::Format("Rank = %d, File = %d\n", board[0][0].getRank(), board[0][0].getFile());
-    // testOut += wxString::Format("Rank = %d, File = %d\n", board[5][3].getRank(), board[5][3].getFile());
-    // testOut += wxString::Format("Rank = %d, File = %d\n", board[7][7].getRank(), board[7][7].getFile());
 
     //basic Frame or Window
-    frame = new wxFrame(nullptr, wxID_ANY, "JustAnotherChessEngine", wxDefaultPosition, wxSize(800, 600));
+    frame = new wxFrame(nullptr, wxID_ANY, "Just Another Chess Engine", wxDefaultPosition, wxSize(800, 800));
 
     //create new wxPanel object
     // wxPanel* panel = new wxPanel(frame);
@@ -34,20 +28,6 @@ bool App::OnInit()
     //create new wxBoxSizer object
     mainSizer = new wxBoxSizer(wxVERTICAL);
     frame->SetSizer(mainSizer);
-
-    // Display board info in a read-only text box
-    // wxTextCtrl* output = new wxTextCtrl(panel, wxID_ANY, testOut, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
-
-    // // Add a text label
-    // wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Hello, World!");
-    //
-    // // Add a button
-    // wxButton* button = new wxButton(panel, wxID_ANY, "Where button");
-    //
-    // //implement button event
-    // button->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
-    //     wxMessageBox("Button was clicked!");
-    // });
 
     // MainMenu* mainMenu = new MainMenu(panel);
 
@@ -59,7 +39,7 @@ bool App::OnInit()
     // sizer->Add(button, 0, wxALL, 10);  // 10px margin around button
     // sizer->Add(boardPanel, 1, wxEXPAND | wxALL, 10);
     // sizer->Add(output, 1, wxEXPAND | wxALL, 10);
-    // panel->SetSizer(sizer);
+    // currentScene->SetSizer(mainSizer);
 
     frame->Show();
     return true;
@@ -68,7 +48,7 @@ bool App::OnInit()
 void App::showScene(wxPanel* scene) {
     if (currentScene) {
         mainSizer->Detach(currentScene);
-        currentScene->Destroy();
+        // currentScene->Destroy();
     }
 
     currentScene = scene;
--- MainMenu.cpp
+++ MainMenu.cpp
@@ -5,19 +5,18 @@
 #include "MainMenu.h"
 
 
-MainMenu::MainMenu(wxWindow *parent, App *app) {
+MainMenu::MainMenu(wxWindow *parent, App *app) : wxPanel(parent), app(app) {
 
     options.push_back("Singleplayer");
     options.push_back("Two player");
     options.push_back("Settings");
 
-    int counter = 0;
-    for (wxString option : options) {
-        buttons.push_back(new wxButton(this, wxID_ANY, option));
-        buttons[counter]->Bind(wxEVT_BUTTON, &MainMenu::onOption, this);
-    }
-
     wxGridSizer* list = new wxGridSizer(buttons.size(), 1, 0, 0);
+    for (wxString option : options) {
+        wxButton* btn = new wxButton(this, wxID_ANY, option);
+        btn->Bind(wxEVT_BUTTON, &MainMenu::onOption, this);
+        buttons.push_back(btn);
+    }
     for (int i = 0; i < buttons.size(); i++) {
         list->Add(buttons[i], 0, wxEXPAND);
     }
