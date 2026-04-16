98cec65826d7515e7fdab2d55fa81a293bf532b5
Cleiton Ribeiro <Cleiton052001@gmail.com>
2026-04-16 17:10:27 -0400

    Adding Knight.cpp

---
+++ Knight.cpp
@@ -0,0 +1,44 @@
+#include "Knight.h"
+#include "Board.h"
+#include "Tile.h"
+#include <iostream>
+
+Knight::Knight(Board* board, int rank, int file, std::string name)
+    : Piece(board, rank, file, name), board(board)
+{
+    board->getTile(rank, file)->setPiece(this);
+}
+
+void Knight::calculateMoves() {
+    validMoves.clear();
+
+    int moves[8][2] = {
+        {2, 1},
+        {2, -1},
+        {-2, 1},
+        {-2, -1},
+        {1, 2},
+        {1, -2},
+        {-1, 2},
+        {-1, -2}
+    };
+
+    for (auto& move : moves) {
+        int r = rank + move[0];
+        int f = file + move[1];
+
+        if (r >= 1 && r <= 8 && f >= 1 && f <= 8) {
+            Tile* tile = board->getTile(r, f);
+            \
+            if (tile->getPiece() == nullptr) {
+                validMoves.push_back(tile);
+            }
+            else {
+
+                if (tile->getPiece()->getColor() != this->getColor()) {
+                    validMoves.push_back(tile);
+                }
+            }
+        }
+    }
+}
