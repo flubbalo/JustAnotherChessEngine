ba0202533e9b8e2bb8540a1ce6fc5544750ae4a1
Cleiton Ribeiro <Cleiton052001@gmail.com>
2026-04-16 16:59:37 -0400

    Adding Bishop

---
+++ Bishop.cpp
@@ -0,0 +1,48 @@
+#include "Bishop.h"
+#include "Board.h"
+#include "Tile.h"
+#include <iostream>
+
+Bishop::Bishop(Board* board, int rank, int file, std::string name)
+    : Piece(board, rank, file, name), board(board)
+{
+    board->getTile(rank, file)->setPiece(this);
+}
+
+void Bishop::calculateMoves() {
+    validMoves.clear();
+
+    int directions[4][2] = {
+        {1, 1},     
+        {1, -1},    
+        {-1, 1},    
+        {-1, -1}    
+    };
+
+    for (auto& dir : directions) {
+        int r = rank;
+        int f = file;
+
+        while (true) {
+            r += dir[0];
+            f += dir[1];
+
+            if (r < 1 || r > 8 || f < 1 || f > 8)
+                break;
+
+            Tile* tile = board->getTile(r, f);
+
+            if (tile->getPiece() == nullptr) {
+                validMoves.push_back(tile);
+            }
+            else {
+
+                if (tile->getPiece()->getColor() != this->getColor()) {
+                    validMoves.push_back(tile);
+                }
+
+                break;
+            }
+        }
+    }
+}
