50ded15bc0a298facf20df045b296a618f2d1ac7
Cleiton Ribeiro <Cleiton052001@gmail.com>
2026-04-16 17:04:12 -0400

    Adding Queen

---
+++ Queen.h
@@ -0,0 +1,18 @@
+#ifndef QUEEN_H
+#define QUEEN_H
+
+#include "Piece.h"
+#include <string>
+
+class Board;
+
+class Queen : public Piece {
+private:
+    Board* board;
+
+public:
+    Queen(Board* board, int rank, int file, std::string name);
+    void calculateMoves() override;
+};
+
+#endif
