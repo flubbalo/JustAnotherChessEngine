e7956668b9e70ac4f15b3a34e9d2be2a0744fd9e
Dylan <reachdylan@gmail.com>
2026-04-16 12:16:34 -0400

    Added comments for move Piece functions in Piece

--- Piece.h
+++ Piece.h
@@ -41,7 +41,9 @@ public:
     int getTeam();
     std::string getTeam(std::string x);
     std::string getName();
+    //move piece function for use with command line interface
     void movePiece();
+    //move piece function for use with UI in main program, old function is technically deprecated
     void movePiece(Tile* tileOrigin, Tile* tileTarget);
 
     //setters
