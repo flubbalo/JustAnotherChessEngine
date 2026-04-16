87977e0ff10aca1e70726937970af0e58b7cf169
Cleiton Ribeiro <Cleiton052001@gmail.com>
2026-04-16 17:05:26 -0400

    Adding King

---
+++ King.cpp
@@ -0,0 +1,44 @@
+#include "King.h"
+#include "Board.h"
+#include "Tile.h"
+#include <iostream>
+
+King::King(Board* board, int rank, int file, std::string name)
+    : Piece(board, rank, file, name), board(board)
+{
+    board->getTile(rank, file)->setPiece(this);
+}
+
+void King::calculateMoves() {
+    validMoves.clear();
+
+    int directions[8][2] = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1},
+        {1, 1},
+        {1, -1},
+        {-1, 1},
+        {-1, -1}
+    };
+
+    for (auto& dir : directions) {
+        int r = rank + dir[0];
+        int f = file + dir[1];
+
+        if (r >= 1 && r <= 8 && f >= 1 && f <= 8) {
+            Tile* tile = board->getTile(r, f);
+
+            if (tile->getPiece() == nullptr) {
+                validMoves.push_back(tile);
+            }
+            else {
+
+                if (tile->getPiece()->getColor() != this->getColor()) {
+                    validMoves.push_back(tile);
+                }
+            }
+        }
+    }
+}
